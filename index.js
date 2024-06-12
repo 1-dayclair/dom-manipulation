16702c437165bf7554fd6a4b69e7009f848e2580
super-time1 <168475235+super-time1@users.noreply.github.com>
2024-06-12 13:23:52 -0400

    index.js

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ first.appendChild(document.createElement("h1")).textContent = "SLIP!"
 first.style.color = "green";
 first.style.textAlign = "center";
 
-const action = document.querySelector("#action");
+const action = document.querySelector("#action"); 
 
 function changeImage() {
     const image = document.createElement("img");
-    image.src = "img/IMG_5676_A.jpg";
+    image.src = "img/IMG_5676_A.jpg"
     first.appendChild(image);
     image.style.height = "267px";
-    image.style.width = "300px"
+    image.style.width = "300px";
+    
+    const image1 = document.createElement("img");
+    image1.src = "img/a-as-in-nuclear-family.jpg" 
+    first.appendChild(image1);
+    image1.style.height = "267px";
+    image1.style.width = "300px";
+    first.appendChild(document.createElement("h5")).textContent = "as in Anchor";
 }
-action.addEventListener("click", changeImage); 
+action.addEventListener("click", changeImage);
 
 const quickWriting = document.getElementById("diamond-mine");
 
